db8ded322b045f16f0f7a316200bc66336724c1d
Tyler Biswell <tsbiswell@gmail.com>
2019-12-12 10:58:15 -0700

    converted JS to JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import { render } from "react-DOM";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Jasmine",
-      animal: "Dog",
-      breed: "Healer"
-    }),
-    React.createElement(Pet, {
-      name: "Blue",
-      animal: "Dog",
-      breed: "Mixed Healer"
-    }),
-    React.createElement(Pet, {
-      name: "Eevee",
-      animal: "Cat",
-      breed: "Seimese"
-    })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Jasmine" animal="Dog" breed="Healer" />
+      <Pet name="Blue" animal="Dog" breed="Mixed Healer" />
+      <Pet name="Eevee" animal="Cat" breed="Siamese" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
